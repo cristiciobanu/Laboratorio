77eede7c4c2f763560584fab29ee83417974e392
Ciobanu C <cristi.ciobanu@scai-consulting.grupposcai.it>
2018-02-07 16:47:33 +0100

    top

--- Prove/js/main.js
+++ Prove/js/main.js
@@ -11,18 +11,23 @@ var STATO_LAMPADINA_1	= 'lampadina1accesa';
 var STATO_LAMPADINA_2	= 'lampadina2accesa';
 
 // EVENTS
+//Change lamp 1 status
 $interruttore1.on('click', function()
 {
   _manageClass(STATO_LAMPADINA_1);
 });
 
+//Change lamp2 status
 $interruttore2.on('click', function()
 {
   _manageClass(STATO_LAMPADINA_2);
 });
 
+//Change all lamp status
 $interruttoreGenerale.on('click', function()
 {
 	_manageClass(STATO_LAMPADINA_1);
   _manageClass(STATO_LAMPADINA_2);
+	$interruttore1.prop('checked', !$interruttore1.prop('checked'));
+	$interruttore2.prop('checked', !$interruttore2.prop('checked'));
 });
