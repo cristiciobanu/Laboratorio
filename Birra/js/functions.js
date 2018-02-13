0fbc591681443ebd6a00853531271ddd3f4ec0b7
Ciobanu C <cristi.ciobanu@scai-consulting.grupposcai.it>
2018-02-13 14:25:35 +0100

    lazy loading images

--- Birra/js/functions.js
+++ Birra/js/functions.js
@@ -1,63 +1,74 @@
 let GestioneBirre = (function () {
 
-  /* DECLARING VARIABLES */
-  let $wrapper, $birra, $input;
-
-  let bflag = 1;
+	/* DECLARING VARIABLES */
+	let $wrapper, $birra, $input, actualPage;
 
 	const INIT_DOC_H = $(document).height();
 
 	const API_ROOT = 'https://api.punkapi.com/v2',
-	API_BEERS = '/beers?page=1&per_page=5';
+	API_BEERS = '/beers?per_page=5&page=';
 
 	let TEMPLATE_BASE_BIRRA =
-		'<article class="birra">'
-		+'<img src="" />'
-		+'<h2></h2>'
-		+'</article>';
+	'<article class="birra">'
+	+'<img src="" />'
+	+'<h2></h2>'
+	+'</article>';
 
 	let TEMPLATE_DETTAGLI_BIRRA =
-		'<section>'
-		+'<h4></h4>'
-		+'<h4></h4>'
-		+'</section>';
+	'<section>'
+	+'<h4></h4>'
+	+'<h4></h4>'
+	+'</section>';
 
-  /* CACHING VARIABLES */
-  function _setup() {
+	/* CACHING VARIABLES */
+	function _setup() {
 		$wrapper = $('#wrapper_birre');
 		$birra = $('.birra');
-    $input = $("#scelta");
-  };
+		$input = $("#scelta");
 
-  /* PRIVATE BUSINESS FUNCTIONS */
-  const _checkEnter = function(e, $this) {
-    if ($this.val() == "" && e.keyCode == 13) {
-      _updateLista();
-    }
+		actualPage = 1;
+	};
 
-  }
+	/* PRIVATE BUSINESS FUNCTIONS */
+	const _checkEnter = function(e, $this) {
+		if ($this.val() == "" && e.keyCode == 13) {
+			_updateLista();
+		}
+
+	}
 
 	const _updateLista = function() {
-		_visualizzaBirre(API_ROOT + API_BEERS).then(
-      function(response) {
-  			response.forEach(function(element, index){
-  				let $birra_base = $(TEMPLATE_BASE_BIRRA);
+		_visualizzaBirre(API_ROOT + API_BEERS + actualPage).then(
+			function(response) {
+				response.forEach(function(element, index){
+					let $birra_base = $(TEMPLATE_BASE_BIRRA);
 
-  				$birra_base.find('img').attr('src', element.image_url);
-  				$birra_base.find('h2').append(element.name);
+					$birra_base.find('img').attr('data-src', element.image_url);
+					$birra_base.find('img').attr('src', 'images/white.png');
+					$birra_base.find('h2').append(element.name);
 
-  				$wrapper.append($birra_base);
-  			});
-      },
-      function(error) {
-        console.error("Failed!", error);
-      }
-    );
-  };
+					$wrapper.append($birra_base);
 
-  const _visualizzaBirra = function() {
+					++actualPage;
+				});
+			},
+			function(error) {
+				console.error("Failed!", error);
+			}
+		);
+	};
+
+	const _swap = function(element) {
+
+		let src = element.attr('src');
+		let dataSrc = element.attr('data-src');
+		element.attr('src', dataSrc);
+		element.attr('dataSrc', src);
+	}
+
+	const _visualizzaBirra = function() {
 		// aggiunge dettagli alla singola birra
-  };
+	};
 
 	const _visualizzaBirre = function(url) {
 
@@ -92,39 +103,45 @@ let GestioneBirre = (function () {
 		});
 
 	};
-  /* END PRIVATE BUSINESS FUNCTIONS */
+	/* END PRIVATE BUSINESS FUNCTIONS */
 
-  /* DECLARING EVENT HANDLER */
-  function _setObserver() {
+	/* DECLARING EVENT HANDLER */
+	function _setObserver() {
+		$input.keypress(function(e) {
+			let $this = $(this)
+			_checkEnter(e, $this);
+		});
 
-    $input.keypress(function(e) {
-      let $this = $(this)
-      _checkEnter(e, $this);
-    });
+		$(window).scroll(function() {
+			if($(window).scrollTop() + INIT_DOC_H >= $(document).height()) _updateLista();
 
-    $(window).scroll(function() {
-      if ($(window).scrollTop() + INIT_DOC_H >= $(document).height()) _updateLista();
-    })
+			$('img').each(function() {
+				let $this = $(this);
 
+				if ($this.isOnScreen()) {
+					_swap($this);
+				}
 
-    // _updateLista();
-    $wrapper.on('hover', $birra, _visualizzaBirra);
+			});
 
-    };
+		})
 
-  function _init() {
-    try {
-      _setup();
-      _setObserver();
-    }
-    catch(e) {
-        console.log('%c ' + e.message, 'color:red');
-        console.log('%c ' + e.stack, 'background: #222; color: #bada55');
-    }
-  }
+		$wrapper.on('hover', $birra, _visualizzaBirra); // dettagli birra
+	};
 
-  return {
-    start: _init
-  };
+	function _init() {
+		try {
+			_setup();
+			_setObserver();
+		}
+		catch(e) {
+			console.log('%c ' + e.message, 'color:red');
+			console.log('%c ' + e.stack, 'background: #222; color: #bada55');
+		}
+	}
+
+	return {
+		start: _init
+	};
 
 })();
