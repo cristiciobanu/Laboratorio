3560f86c48a375fa20ce1f0d6c8d89c96d167071
Ciobanu C <cristi.ciobanu@scai-consulting.grupposcai.it>
2018-02-14 13:41:55 +0100

    no message

--- Birra/Gruntfile.js
+++ Birra/Gruntfile.js
@@ -2,11 +2,18 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-    // Tasks
+		// Tasks
 		sass: {
 			dist: {
 				files: {
 					'css/style.css' : 'sass/style.scss'
+				}
+			}
+		},
+		babel: {
+			dist: {
+				files: {
+					"dist/app.js": "src/app.js"
 				}
 			}
 		},
@@ -17,9 +24,10 @@ module.exports = function(grunt) {
 			}
 		}
 	});
-  // Load Grunt plugins
+	// Load Grunt plugins
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-  // Register Grunt tasks
+	grunt.loadNpmTasks('grunt-babel');
+	// Register Grunt tasks
 	grunt.registerTask('default',['watch']);
 }
